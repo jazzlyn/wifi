7a3a9f51ceabb6a2cff428b84824c8c6df4195c1
Jazzlyn <jasmin.zesch@gmail.com>
2016-06-22 16:46:30 +0200

    added select and checkbox

--- 07_forms/js/forms.js
+++ 07_forms/js/forms.js
@@ -10,41 +10,155 @@
   console.log(loginForm.elements.password);*/
   var username = document.getElementById('userName');
   var password = document.getElementById('password');
+  var service = document.getElementById('service');
+  var checkbox = document.getElementById('agb');
 
   loginForm.addEventListener('submit', function(e) {
-    e.preventDefault;
     if (validateForm() === 0) {
       alert('okay');
     } else {
-       wrongInput();
+      e.preventDefault();
+      if (username.value === '') {
+        getError(username);
+      }
+      if (password.value === '') {
+        getError(password);
+      }
+      if (service.value === '') {
+        getError(service);
+      }
+      if (checkbox.checked === false) {
+        getError(checkbox);
+      }
     }
   });
 
 
   function validateForm() {
-    var errors = 2;
+  // add blur event for live validating
+  var errors = 4;
     if (username.value !== '') {
       errors--;
-      deleteErrorMessage();
+      deleteError(username);
     }
     if (password.value !== '') {
       errors--;
-      deleteErrorMessage();
+      deleteError(password);
+    }
+    if (service.value !== '') {
+      errors--;
+      deleteError(service);
+    }
+    if (checkbox.checked === true) {
+      errors--;
+      deleteError(checkbox);
     }
     return errors;
   };
 
-  function wrongInput() {
-    var errorElement = document.createElement('div');
+
+  function getError(element) {
+    var errorElement = document.createElement('p');
+    var text = document.createTextNode('Bitte füllen Sie das Feld aus!');
+    errorElement.appendChild(text);
     errorElement.className = 'form-error';
-    errorElement.innerHTML = 'Bitte füllen Sie das Feld aus!';
-    username.insertAdjacentHTML('afterend', errorElement);
+    element.parentNode.insertBefore(errorElement, element);
   }
 
-  function deleteErrorMessage() {
-    var errorClass = username.nextElementSibling;
+  function deleteError(element) {
+    var errorClass = element.nextElementSibling;
     if (errorClass.classList.contains('form-error')) {
-      username.parentNode.removeChild(errorClass);
+      element.parentNode.removeChild(errorClass);
     }
   }
 })();
+
+/*(function () {
+
+    var errorRequired = 'Bitte füllen Sie das Feld aus.';
+    var errorSelectRequired = 'Bitte wählen Sie einen Wert aus.';
+    var errorCheckbox = 'Bitte bestätigen Sie';
+
+    var userName = document.getElementById('userName');
+    var password = document.getElementById('password');
+    var service = document.getElementById('service');
+    var agb = document.getElementById('agb');
+
+    loginForm.addEventListener('submit', function (e) {
+        // Best practice: ein Formular ist ungültig und muss erst beweisen, dass es gültig ist.
+        var errors = 4; // Anzahl der möglichen Fehler
+
+        if (userName.value !== '') {
+            errors--;
+            removeError(userName);
+        } else {
+            addError(userName, errorRequired);
+        }
+
+        if (password.value !== '') {
+            errors--;
+            removeError(password);
+        } else {
+            addError(password, errorRequired);
+        }
+
+        if (service.options[service.selectedIndex].value !== '') {
+            errors--;
+            removeError(service);
+        } else {
+            addError(service, errorSelectRequired);
+        }
+
+        if (agb.checked === true) {
+            errors--;
+            removeError(agb);
+        } else {
+            addError(agb, errorCheckbox);
+        }
+
+        if (errors !== 0) {
+            e.preventDefault();
+        }
+    });
+
+    function addError(elem, errorMsg) {
+        var myError = getError(elem);
+        myError.innerHTML = errorMsg;
+
+        // Checkboxen (und später Radio Buttons) gesondert behandeln, Fehler nach label
+        var where = elem;
+        if (elem.type === 'checkbox') {
+            where = document.querySelector('label[for=' + elem.id + ']');
+        }
+        where.insertAdjacentElement('afterend', myError);
+    }
+
+    function getError(elem) {
+        // Check, ob error bereits vorhanden ist
+        var nextElement = elem.nextElementSibling;
+
+        if (nextElement !== null && nextElement.classList.contains('form-error')) {
+            return nextElement;
+        }
+
+        // Erstellen, falls es noch nicht vorhanden ist
+        var myError = document.createElement('div');
+        myError.className = 'form-error';
+        return myError;
+    }
+
+    function removeError(elem) {
+        // Checkboxen müssen ein wenig anders behandelt werden
+        var where = elem;
+        if (elem.type === 'checkbox') {
+            where = document.querySelector('label[for=' + elem.id + ']');
+        }
+        var nextElement = where.nextElementSibling;
+
+        if (nextElement !== null && nextElement.classList.contains('form-error')) {
+            // Kann nur vom Elternelement aus gelöscht werden.
+            nextElement.parentNode.removeChild(nextElement);
+        }
+    }
+
+}());
