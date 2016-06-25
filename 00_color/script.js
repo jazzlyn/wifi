75bf05028fdec8b552f955c42791a65a24e31286
jazzlyn <jasmin.zesch@gmail.com>
2016-06-25 12:44:21 +0200

    restructured directories

--- 00_color/script.js
+++ 00_color/script.js
@@ -4,7 +4,7 @@ for (var i = 0; i < allButtons.length; i++) {
   allButtons[i].addEventListener("click", changeColor);
 };
 
-function changeColor() {
+function changeColor(event) {
   var actualColor = event.target.id;
   var newColor = actualColor.slice(4);
   document.getElementById("colorfield").style.backgroundColor = newColor;
