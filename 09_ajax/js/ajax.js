1073bc284b063ed057108ea759344ecb1b5d87b7
Jazzlyn <jasmin.zesch@gmail.com>
2016-06-29 15:13:53 +0200

    minor fixes

--- 09_ajax/js/ajax.js
+++ 09_ajax/js/ajax.js
@@ -2,11 +2,10 @@
     'use strict';
     /*
     Same Domain Policy: Die von XHR aufgerufenen URLs müssen sich unter der selben
-    Domain wie der Aufrufer befinden. AJAX kann daher nicht über das File-System
+    Domain wie der Aufrufer befinden. AJAX kann daher nicht über das Filesystem
     getestet werden (Doppelklick auf Datei), da das Filesystem keine Domain ist.
 
-    AJAX muss immer unter einem Webserver laufen. Brackets zb. lässt uns über die
-    Live Vorschau die Dateien in einem einfache Webserver aufrufen.
+    AJAX muss immer unter einem Webserver laufen.
     */
     var button = document.querySelector('button');
 
@@ -18,7 +17,7 @@
         var url = 'answer.html';
         var xhr = new XMLHttpRequest(); // state 0 object wird erstellt
         /*
-        Wir geben bekannt, welche Funktion aufgerufen wird, wenn sich der Status des xhr     Objekts ändert.
+        Wir geben bekannt, welche Funktion aufgerufen wird, wenn sich der Status des xhr Objekts ändert.
 
         0 nicht initialisiert (Objekt erzeugt)
         1 geöffnet (Request wurde gesendet, noch keine Antwort)
@@ -26,19 +25,25 @@
         3 Daten werden übertragen
         4 Request wurde beendet
         */
-        xhr.onreadystatechange = stateChanged;
-        //xhr.onreadystatechange = stateChanged; // trigger, wenn sich status ändert
+        xhr.onreadystatechange = stateChanged; // trigger, wenn sich der Status ändert
         // initialisieren
         xhr.open('GET', url);
         // senden, es werden sich jetzt die States ändern und die Funktion stateChanged aufgerufen
         xhr.send();
     };
-
+    /*
+    Die Callback Funktion ist außerhalb des Kontext des xhr Objekts definiert,
+    deshalb können wir in stateChanged nicht über die xhr Variable darauf
+    zugreifen. Das Event Objekt, das wir in der Variable ev speichern, weiß aber,
+    wer das Event aufrief: ev.currentTarget. Das ist der Besitzer/Aufrufer des
+    events = xhr im obigen Kontext.
+    */
     function stateChanged(event) {
         if (event.currentTarget.readyState === 4 && event.currentTarget.status === 200) {
             var divNode = document.getElementById('msg');
             divNode.innerHTML = event.currentTarget.response;
         }
     }
+
     init();
 })();
