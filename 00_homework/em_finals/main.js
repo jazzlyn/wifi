477940b2e5eb355bcfc9dfdb1b8f017a6e5d4665
Jazzlyn <jasmin.zesch@gmail.com>
2016-06-29 16:43:09 +0200

    added functions

--- 00_homework/em_finals/main.js
+++ 00_homework/em_finals/main.js
@@ -4,6 +4,8 @@
 erstelle ein Formular das einen em tipp abgeben lässt für den gewinner des finales (select list). hat der user ein land ausgewählt wird der button “tipp abschicken” aktiviert, drückt der user dann da drauf kommt ein dialog: Wir Drücken dir die Daumen das <name der ausgewählten Mannschaft> die em 2016 in Frankreich gewinnt, anstelel des forms. ab hier kann der user nichts mehr machen.
 */
     var form = document.querySelector('form');
+    //var select = form.querySelector('select');
+    var select = document.getElementById('country');
     var submitButton = form.querySelector('button[type=submit]');
 
     function init() {
@@ -20,16 +22,18 @@ erstelle ein Formular das einen em tipp abgeben lässt für den gewinner des fin
     }
 
     function submitHandler(event) {
-        /*if (nothing selected)) {
+
+    }
+  /*
+            if (!select.value === '') {
+            enableSubmit;
+        } else {
             event.preventDefault();
-        }*/
-    }
-
-    function getSelectedValue() {
-        // get value of select field for enable submit function.
-    }
-
+        }
+*/
     function dialogBox() {
+        var countrySelect = select.value;
+        return alert = ('Good Luck! Maybe ' + countrySelect + 'will win!');
         // create dialog box with selected country and
         // 'Good Luck! Maybe ' + countrySelect + 'will win!'
     }
