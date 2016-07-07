5c2fdfb03b26bd855f852c3eeb28fc4d49236d9b
Jazzlyn <jasmin.zesch@gmail.com>
2016-07-07 15:54:51 +0200

    added enter function and code cleaning

--- 12_autocomplete_jq/js/autocomplete_jq.js
+++ 12_autocomplete_jq/js/autocomplete_jq.js
@@ -1,6 +1,4 @@
-var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"];
-
-(function(data) {
+(function() {
     'use strict';
 
     var search = $('#search');
@@ -8,19 +6,22 @@ var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "A
 
     var UP = 38;
     var DOWN = 40;
+    var ENTER = 13;
     
     function init() {
         search.on('keyup', keyupHandler);
+        search.on('keydown', keydownHandler);
     }
 
     function keyupHandler(event) {
-        if (event.keyCode === DOWN) {
+        var keyCode = event.keyCode;
+        if (keyCode === DOWN) {
             var index = getCurrentIndex();
             index++;
             setActive(index);
             return;
         }
-        if (event.keyCode === UP) {
+        if (keyCode === UP) {
             var index = getCurrentIndex();
             index--;
             if (index < 0) {
@@ -29,27 +30,36 @@ var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "A
             setActive(index);
             return;
         }
-        var srch = search.val().toLowerCase();
-        var results = [];
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].toLowerCase().indexOf(srch) === 0) {
-                results.push(data[i]);
-            }
-            if (results.length >= 10) {
-                break;
-            }
-        }
-        createAutoComplete(results);
-        /*var url = 'countries.php';
+
+        var url = 'countries.php';
         $.get(
             url,
             {'srch': search.val()},
             function(data) {
-                console.log(data);
                 createAutoComplete(data);
             },
             'json'
-        );*/
+        );
+    }
+    function keydownHandler(event) {
+        console.log(event);
+        if (event.keyCode === UP || event.keyCode === DOWN) {
+            return;
+        }
+        if (event.keyCode === ENTER) {
+            event.preventDefault();
+            search.val($('.search-result .active').text);
+        }
+
+        var url = 'countries.php';
+        $.get(
+            url,
+            {'srch': search.val()},
+            function(data) {
+                createAutoComplete(data);
+            },
+            'json'
+        );
     }
 
     function createAutoComplete(JSONresponse) {
@@ -59,11 +69,9 @@ var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "A
         }
         ulNode.empty();
         for (var i = 0; i < JSONresponse.length; i++) {
-            var liNode = $('<li></li>')
-                .text(JSONresponse[i])
-                .on('click', function(event) {
-                    search.val( $(this).text() );
-                    ulNode.remove();
+            var liNode = $('<li></li>').text(JSONresponse[i]).on('click', function(event) {
+                search.val( $(this).text() );
+                ulNode.remove();
             });
             ulNode.append(liNode);
         }
@@ -78,12 +86,12 @@ var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "A
     }
     
     function getCurrentIndex() {
-        var cur = document.querySelector('.search-result .active');
-        if (!cur) {
+        var currentNode = document.querySelector('.search-result .active');
+        if (!currentNode) {
             return -1;
         }
-        for (var i = 0; i < cur.parentNode.childNodes.length; i++) {
-            if (cur.parentNode.childNodes[i] === cur) {
+        for (var i = 0; i < currentNode.parentNode.childNodes.length; i++) {
+            if (currentNode.parentNode.childNodes[i] === currentNode) {
                 return i;
             }
         }
@@ -95,5 +103,5 @@ var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "A
     
     init();
 
-})(data);
+})();
 
