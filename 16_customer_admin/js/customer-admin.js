ea5d451e8516a15273a4aef32de48a13a0e177fa
Jazzlyn <jasmin.zesch@gmail.com>
2016-07-13 23:13:47 +0200

    minor fixes

--- 16_customer_admin/js/customer-admin.js
+++ 16_customer_admin/js/customer-admin.js
@@ -7,33 +7,32 @@
         loadJSON();
     }
 
-
     function loadJSON() {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', jsonURL);
+        xhr.open('GET', jsonURL, true);
         xhr.onreadystatechange = stateChanged;
         xhr.send();
     }
 
-    function stateChanged() {
+    function stateChanged(event) {
         if (event.currentTarget.readyState === 4 && event.currentTarget.status === 200) {
-            var jsonResp = JSON.parse(event.currentTarget.response);
-            createTable(jsonResp);
+            var json = JSON.parse(event.currentTarget.response);
+            createTable(json);
         }
     }
 
-    function createTable(jsonResp) {
-        var gridTable = document.createElement('table');
-        gridTable.id = 'gridTable';
-        gridTable.className = 'pure-table pure-table-horizontal';
+    function createTable(json) {
+        var table = document.createElement('table');
+        //table.id = 'table';
+        table.className = 'pure-table pure-table-horizontal';
         // create thead
-        createTHead(jsonResp, gridTable);
+        createTHead(json, table);
         // create tbody for table
         var tbody = document.createElement('tbody');
         // iterate through all objects in json array
-        for (var i = 0; i < jsonResp.length; i++) {
+        for (var i = 0; i < json.length; i++) {
             // get actual object from the array
-            var obj = jsonResp[i];
+            var obj = json[i];
             // create row for each customer
             var row = tbody.insertRow();
             // create cell for each customer
@@ -42,16 +41,16 @@
                 cell.innerHTML = obj[key];
             }
         }
-        gridTable.appendChild(tbody);
-        document.body.appendChild(gridTable);
+        table.appendChild(tbody);
+        document.body.appendChild(table);
     }
 
-    function createTHead(jsonResp, gridTable) {
+    function createTHead(json, table) {
         // creates header for table
-        var thead = gridTable.createTHead();
+        var thead = table.createTHead();
         // inserts a row into the header
         var row = thead.insertRow(0);
-        var firstObj = jsonResp[0];
+        var firstObj = json[0];
         for (var key in firstObj) {
             var cell = row.insertCell();
             var capKey = capitalizeFirstLetter(key);
