6a7c8b31f82492e5e959bb73907d58ef1a4092ab
Jazzlyn <jasmin.zesch@gmail.com>
2016-07-21 15:08:26 +0200

    new homework unit: zodiac signs

---
+++ 00_homework/zodiac_signs/js/main.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+
+})();
+/*
+der user soll sein sternzeichen aus einer liste aller sternzeichen wäheln können, klickt er auf sein sternzeichen soll die liste nicht mehr sichtbar sein und eine ansicht angezigt weden die folgendes html enthält:
+
+<div>
+	<h1>Fische</h1>
+	<p>dein persönliches Horoskop für <span class=”datetime”>Samstag 25.06.2016 13:02:57</span></p>
+	<ul>
+		<li>Glück: 4/5</li>
+		<li>Lieber: 3/5</li>
+		<li>Geld: 5/5</li>
+		<li>Famillie: 2/5</li>
+		<li>Job: 4/5</li>
+		<li>Freizeit: 2/5</li>
+	</ul>
+</div>
+
+im h1 soll das ausgewählte Sternzeichen stehen
+im .datetime das Aktuelle datum mit Uhrzeit, wie oben formatiert
+die werte für Glück, Liebe, etc sollen zufällig erstellt werden.
+Wobei kein wert unter 2 herauskommen darf und die gesamt anzahl der punkte immer 20 ergeben müssen.
+
+der user kann von der “horoskop ansicht” zurück zur sternzeichen liste und ein anderes sternzeichen anwählen oder ein aktuelles horoskop erstellen lassen.
+*/
