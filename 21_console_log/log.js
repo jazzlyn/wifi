aeadd9d88371a609125b4493d624f0ab46e5b416
Jazzlyn <jasmin.zesch@gmail.com>
2016-07-27 11:17:46 +0200

    created custom console log for use in browser

--- 21_console_log/log.js
+++ 21_console_log/log.js
@@ -1,50 +1,42 @@
-/*
-	Dieser Code ist nicht funktionell!!!!!!
-	Module reveal pattern ist die Lösung: z. B.
-	https://www.christianheilmann.com/2007/08/22/again-with-the-module-pattern-reveal-something-to-the-world/
+/*jshint browser: true */
+/*jshint esnext: true */
 
-	https://carldanley.com/js-revealing-module-pattern/
+var Logger = function () {
+    var logList;
+    var history = {};
+    var counter = 0;
+    // public
+    function log(content) {
+        createEntry(content);
+        // write to history object
+    }
+    // private
+    function createEntry (content) {
+        var entry = document.createElement('li');
+        entry.innerHTML = content;
+        logList.appendChild(entry);
+    }
 
-*/
+    function init() {
+        if (logList !== undefined) {
+            logList.parentNode.removeChild(logList);
+        }
 
-var Logger = function(){
-	// public
-	function log (content) {
-		console.log("content");
-		createEntry(content);
-		// write to history object
+        var logger = document.createElement('div');
+        logger.id = "logger";
+        logList = document.createElement('ul');
+        logList.className = 'log-list';
+        var titleBar = document.createElement('h2');
+        titleBar.className = 'titlebar';
+        titleBar.innerHTML = 'Logger';
+        createEntry('Logger initialized');
+        logger.appendChild(titleBar);
+        logger.appendChild(logList);
+        document.body.appendChild(logger);
+    }
 
-	}
-
-	// private
-	var createEntry = function(content, id) {
-		var entry = document.createElement('li');
-		entry.innerHTML = content;
-		entry.id = id;
-		logList.appendChild(entry);
-	}
-
-	var history = {};
-	var counter = 0;
-
-	var logger = document.createElement('div');
-	logger.id = "logger";
-
-	var logList = document.createElement('ul');
-	logList.className = 'log-list';
-
-	var titleBar = document.createElement('h2');
-	titleBar.className = 'titlebar';
-	titleBar.innerHTML = 'Logger';
-
-	createEntry('Logger initialized');
-
-	logger.appendChild(titleBar);
-	logger.appendChild(logList);
-
-	document.body.appendChild(logger);
-
-	return {
-		log: log()
-	}
+    return {
+        log: log,
+        init: init
+    };
 };
