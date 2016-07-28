46db65c19e45255f5e53316053d0c9281f159343
Jazzlyn <jasmin.zesch@gmail.com>
2016-07-28 16:05:16 +0200

    finished ajax module

--- 00_homework/06_ajax_library/js/ajax-library.js
+++ 00_homework/06_ajax_library/js/ajax-library.js
@@ -4,48 +4,102 @@
 (function() {
     'use strict';
     
-    function get(url) {
-        // new promise
-        return new Promise(function(resolve, reject) {
-        // xhr request
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
+    var Params = {
+        method: '', // method of request, GET-POST-PUT-DELETE
+        url: '', // ajax url
+        format: '', // format of string (json or plain accepted)
+        data: [], // data for sending
+    };
 
-        xhr.onload = function() {
-            // check status
-            if (xhr.status === 200) {
-                var response;
-                // resolve promise with response text
-                if (xhr.responseType === "json") {
-                    xhr.setRequestHeader("Accept", "json");
-                    response = JSON.parse(xhr.response);
-                } else if (xhr.responseType === "text") {
-                    response = xhr.response;
-                } else {
-                    return;
-                }
-                resolve(response);
-            } else {
-                // reject promise with status text
-                reject(Error(xhr.statusText));
-            }
-        };
-
-        // handle network errors
-        xhr.onerror = function() {
-            reject(Error("Network Error"));
-        };
-
-        // make the request
-        xhr.send();
+    function get(method, url, format) {
+        return _request({
+            method: 'GET',
+            url: url,
+            format: format
         });
     }
 
-    /* Use it!
-    get('story.json').then(function(json) {
-        console.log("Success!", json);
-    }, function(error) {
-        console.error("Failed!", error);
-    });*/
+    function post(method, url, data) {
+        return _request({
+            method: 'POST',
+            url: url,
+            data: data
+        });
+    }
 
+    function put(method, url, data) {
+        return _request({
+            method: 'PUT',
+            url: url,
+            data: data
+        });
+    }
+
+    function del(method, url, data) {
+        return _request({
+            method: 'DELETE',
+            url: url,
+            data: data
+        });
+    }
+
+    // private function _ !
+    function _request(Params) {
+        // new promise
+        return new Promise(function(resolve, reject) {
+            // xhr request
+            var xhr = new XMLHttpRequest();
+            // open request and get method and url
+            xhr.open(Params.method, Params.url);
+            /*
+              Method 'GET'
+            */
+            if (Params.method === 'GET' && Params.format === 'json') {
+                xhr.setRequestHeader('Accept', 'json');
+            }
+            /*
+              Method 'POST' & 'PUT' & 'DELETE'
+            */
+            if (Params.method === 'POST' || Params.method === 'PUT' || Params.method === 'DELETE') {
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            }
+            // do, when xhr loaded
+            xhr.onload = function() {
+                // check status ok
+                if (xhr.status === 200) {
+                    var response;
+                    /*
+                    Method 'GET'
+                    */
+                    if (Params.method === 'GET') {
+                        if (Params.format === 'json') {
+                            response = JSON.parse(xhr.responseText);
+                        } else if (Params.format === 'plain') {
+                            response = xhr.response;
+                        }
+                    }
+                    // resolve promise with response text
+                    resolve(response);
+                } else {
+                    // reject promise with status text
+                    reject(Error(xhr.statusText));
+                }
+            };
+
+            // handle network errors
+            xhr.onerror = function() {
+                reject(Error('Network Error'));
+            };
+
+            // make the request
+           xhr.send(Params.data); // Params.data optional
+        });
+    }
+    /*
+    _request('data/zodiac.json').then(function(response) {
+        console.log('Success!', response);
+    }, function(error) {
+        console.error('Failed!', error);
+    });
+    */
 })();
